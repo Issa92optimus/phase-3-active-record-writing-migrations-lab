189c0d575299f375fb2b86329c7dc4dececbfee6
Issa Nyale <me@email.com>
2023-02-22 19:18:47 +0300

    completed

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,17 @@
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/students.sqlite"
+  )
+
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+  )
+  SQL
+
+  ActiveRecord::Base.connection.execute(sql)
+
 class Student < ActiveRecord::Base
 end
